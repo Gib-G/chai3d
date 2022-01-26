bcf62421dd3a013acacc7efe34c56cd3af8c468b
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2022-01-26 22:12:14 +0100

    Moved SimpleSerial class declaration inside of namespace chai3d; Removed using statements for std in SimpleSerial.h

--- src/arduino/SimpleSerial.h
+++ src/arduino/SimpleSerial.h
@@ -1,3 +1,5 @@
+#ifndef CHAI3D_SIMPLE_SERIAL_H
+#define CHAI3D_SIMPLE_SERIAL_H
 
 #include <Windows.h>
 #include <stdio.h>
@@ -10,28 +12,32 @@
 #include <time.h>
 #include <fstream>
 
-using namespace std;
+namespace chai3d {
 
-class SimpleSerial
-{
+	class SimpleSerial
+	{
 
-private:
-	HANDLE io_handler_;
-	COMSTAT status_;
-	DWORD errors_;
+	private:
+		HANDLE io_handler_;
+		COMSTAT status_;
+		DWORD errors_;
 
-	string syntax_name_;
-	char front_delimiter_;
-	char end_delimiter_;
+		std::string syntax_name_;
+		char front_delimiter_;
+		char end_delimiter_;
 
-	void CustomSyntax(string syntax_type);
+		void CustomSyntax(std::string syntax_type);
 
-public:
-	SimpleSerial(char* com_port, DWORD COM_BAUD_RATE);
+	public:
+		SimpleSerial(char* com_port, DWORD COM_BAUD_RATE);
 
-	string ReadSerialPort(int reply_wait_time);
-	bool WriteSerialPort(char* data_sent);
-	bool CloseSerialPort();
-	~SimpleSerial();
-	bool connected_;
-};
+		std::string ReadSerialPort(int reply_wait_time);
+		bool WriteSerialPort(char* data_sent);
+		bool CloseSerialPort();
+		~SimpleSerial();
+		bool connected_;
+	};
+
+}
+
+#endif
